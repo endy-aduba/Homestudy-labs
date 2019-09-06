186eb50a3bfffc6077bc16255ba14414bd88ace9
endy <endyaduba@gmail.com>
2019-09-06 01:52:59 +0100

    wordCount

--- WordCount/WordCountSolution.js
+++ WordCount/WordCountSolution.js
@@ -15,17 +15,17 @@
 // module.exports = wordCount;
 
 function wordCount(arrayOfStrings) {
-    let newmap = [];
+    let newobj = {};
     let arr = [];
     
     let fish;
     for(let i = 0; i < arrayOfStrings.length; i++){
       if(arr.includes(arrayOfStrings[i]) != true){
       fish = arrayOfStrings.filter(x => x == arrayOfStrings[i]);
-        newmap.push(`{${arrayOfStrings[i]} : ${fish.length}}`);
+      newobj[arrayOfStrings[i]] = fish.length;
     }
   }
-    return newmap;
+    return newobj;
   }
 
 module.exports = wordCount;
