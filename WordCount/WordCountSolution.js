7b8d8602951515079e640909f4ad013909351423
endy <endyaduba@gmail.com>
2019-09-06 01:35:47 +0100

    homestudy

--- WordCount/WordCountSolution.js
+++ WordCount/WordCountSolution.js
@@ -1,5 +1,31 @@
-function wordCount(arrayOfStrings) {
+// function wordCount(arrayOfStrings) {
+//     let newmap = new Map();
+//     let arr = [];
+    
+//     let fish;
+//     for(let i = 0; i < arrayOfStrings.length; i++){
+//       if(arr.includes(arrayOfStrings[i]) != true){
+//       fish = arrayOfStrings.filter(x => x == arrayOfStrings[i]);
+//         newmap.set(arrayOfStrings[i],fish.length);
+//     }
+//   }
+//     return newmap;
+//   }
 
-}
+// module.exports = wordCount;
+
+function wordCount(arrayOfStrings) {
+    let newmap = [];
+    let arr = [];
+    
+    let fish;
+    for(let i = 0; i < arrayOfStrings.length; i++){
+      if(arr.includes(arrayOfStrings[i]) != true){
+      fish = arrayOfStrings.filter(x => x == arrayOfStrings[i]);
+        newmap.push(`{${arrayOfStrings[i]} : ${fish.length}}`);
+    }
+  }
+    return newmap;
+  }
 
 module.exports = wordCount;
--- WordLength/WordLengthSolution.js
+++ WordLength/WordLengthSolution.js
@@ -1,6 +1,15 @@
 function wordLength(arr) {
-//code goes here
-
-}
-
+    //code goes here
+    let finalmap = new Map();
+    let newarr = [];
+      for(let i = 0;i<arr.length; i++){
+          if(newarr.indexOf(arr[i]) == -1){
+              newarr.push(arr[i]);
+          }
+      }  
+      for(let p =0;p<newarr.length; p++){
+        finalmap.set(newarr[p].toLowerCase(),newarr[p].length);
+      }  
+        return finalmap;
+      }
 module.exports = wordLength;
